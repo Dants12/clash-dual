3928f38174906687ab166e24fda099809c9cb78c
Dants12 <Anton.didenko@gmail.com>
2025-09-21 18:54:37 +0300

    Update ws.ts

--- client/src/ws.ts
+++ client/src/ws.ts
@@ -2,9 +2,11 @@ export type WS = ReturnType<typeof createWS>;
 
 
 export function createWS(onMsg: (m:any)=>void) {
-const url = (location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host.replace(/:\d+$/,'') + ':8081';
-const ws = new WebSocket(url);
-ws.onopen = () => ws.send(JSON.stringify({ t: 'auth' }));
-ws.onmessage = (ev) => onMsg(JSON.parse(ev.data));
-return ws;
+  const protocol = location.protocol === 'https:' ? 'wss:' : 'ws:';
+  const host = location.host;
+  const ws = new WebSocket(`${protocol}//${host}/ws`);
+  ws.onopen = () => ws.send(JSON.stringify({ t: 'auth' }));
+  ws.onmessage = (ev) => onMsg(JSON.parse(ev.data));
+  return ws;
 }
+
