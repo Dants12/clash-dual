f88626359c7cfcd245c60e939107b78ef4f4cd95
Dants12 <Anton.didenko@gmail.com>
2025-09-23 15:32:49 +0300

    Fix client build by removing stale stylesheet import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './app.css';
-import './styles/app.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
